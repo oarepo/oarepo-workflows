819bd58d49b7df5d52bb4b052071e8496751cf3d
ron <kristron6@gmail.com>
2026-02-10 12:46:59 +0100

    allowing creation of records without a workflow

--- oarepo_workflows/services/components/workflow.py
+++ oarepo_workflows/services/components/workflow.py
@@ -14,8 +14,6 @@ from typing import TYPE_CHECKING, Any, override
 from invenio_records_resources.services.records.components.base import ServiceComponent
 from oarepo_runtime.typing import require_kwargs
 
-from oarepo_workflows.errors import MissingWorkflowError
-
 if TYPE_CHECKING:
     from flask_principal import Identity
     from invenio_drafts_resources.records import Record
@@ -49,19 +47,9 @@ class WorkflowComponent(ServiceComponent):
     ) -> None:
         """Implement record creation checks and set the workflow on the created record."""
         if not data:
-            # sanity check, should be handled by policy before the component is called
-            raise MissingWorkflowError(
-                "Workflow not defined in input. As this should be handled by a policy, "
-                "make sure you are using workflow-enabled policy.",
-                record=data,
-            )  # pragma: no cover
+            return
         try:
             workflow_id = data["parent"]["workflow"]
-        except KeyError as e:  # pragma: no cover
-            # sanity check, should be handled by policy before the component is called
-            raise MissingWorkflowError(  # pragma: no cover
-                "Workflow not defined in input. As this should be handled by a policy, "
-                "make sure you are using workflow-enabled policy.",
-                record=data,
-            ) from e
+        except KeyError:  # pragma: no cover
+            return
         record.parent.workflow = workflow_id  # type: ignore[reportAttributeAccessIssue, reportOptionalMemberAccess]
